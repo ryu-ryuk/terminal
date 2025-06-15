8dde3a06461c8169022a58c2e9b9fc831d276ae5
ryu-ryuk <alokraj3850@gmail.com>
2025-06-16 01:49:52 +0530

    safelisting for dots

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    'card-dot',
+    'experience-card',
+    'experience-line',
+    'transform',
+    '-translate-x-2',
+    'rounded-full',
+    'w-4',
+    'h-4',
+    'absolute',
+    'left-4',
+    'bg-green-400'
+  ],
   theme: {
     extend: {
+      transitionProperty: {
+        width: 'width'
+      },
       colors: {
         terminalGreen: "#33FF33",
         terminalBackground: "#000000",
